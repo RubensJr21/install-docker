2f05e2a15e3e8c0f838bbdf297e15f96f4915e38
RubensJr21 <50061129+RubensJr21@users.noreply.github.com>
2022-06-05 22:23:25 -0300

    Update install.sh
    Add the new variable name to user ($USER => $1)

--- install.sh
+++ install.sh
@@ -6,4 +6,5 @@ add-apt-repository "deb [arch=amd64] https://download.docker.com/linux/ubuntu fo
 apt update;
 apt install -y docker-ce docker-compose-plugin docker-compose;
 echo -e "[boot]\ncommand = service docker start" >> /etc/wsl.conf;
-usermod -aG docker $USER;
+# Será passado o nome do usuário pelo comando que irá chamar o script
+usermod -aG docker $1;
